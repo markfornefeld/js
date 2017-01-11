d888adfe7bddd1b69e663b71c93abeeefca6a248
James <jhandshoe@sbcglobal.net>
2017-01-11 11:13:25 -0500

    added int and var js files

---
+++ 0.01_integers.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+console.log(1 + 2);
+console.log(100 / 5);
+console.log(5 % 2);
+
+console.log(36);
+console.log(1980);
+console.log(1);
+console.log(2);
+
+console.log(5 % 2);
+console.log(11 % 3);
+console.log(30 % 18);
+
+console.log(Math.pow(10,2));
+
---
+++ 0.02_variables.js
@@ -0,0 +1,55 @@
+/********Variables *******/
+
+//this variable is a string type
+var firstname = "James";
+var lastname = "Handshoe"
+var fullname = firstname + " " + lastname;
+
+//concatenation (this is where you can build or combine strings)
+console.log(firstname + lastname);
+console.log(fullname);
+
+// empty variables
+
+//variable declaration or initialization
+var num;
+//variable assignment
+var num = 2;
+console.log(num);
+
+//declare an empty string variable
+var petsName = "";
+petsName = "Jones the blind dog";
+console.log(petsName);
+
+//conventional variable namings
+//camel case (petsName) - first word is lower cased the next words are upper
+var packersScoreThisWeekend = 38;
+
+//snake case - each word is connected with an _
+var packers_score_from_last_weekend = 35;
+
+
+//shorthand operators
+
+var age = 40;
+//reassigning variable
+//age += 1 is the same as age = age + 1
+console.log(age += 1);
+
+//age = age / 2
+console.log(age /= 2);
+
+//age = age * 2
+console.log(age *= 2);
+
+//age = age - 1
+console.log(age -= 1);
+
+
+
+
+
+
+
+
